e7850b16dad5bed5588ba82ab31a2acca81a77d0
rmorgan13 <104103933+rmorgan13@users.noreply.github.com>
2022-10-30 16:15:36 -0400

    started api stuff

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const characterRoutes = require('./characterRoutes');
+const partyRoutes = require('./partyRoutes');
+const inventoryRoutes = require('./inventoryRoutes');
 
-const userRoutes = require('../userRoutes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
+router.use('/character', characterRoutes);
+router.use('/party', partyRoutes);
+router.use('/invetory', inventoryRoutes);
 
 module.exports = router;
