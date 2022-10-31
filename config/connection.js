7d360f3a3851598629800745697d35e242ce7b85
jlamb88 <112279704+jlamb88@users.noreply.github.com>
2022-10-31 12:31:33 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,10 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  'page_holding_db',
-  'root',
-  '99441378Cd',
+  
   {
     host: "localhost",
     dialect: "mysql",
     port: 3306,
   }
 );
-
-module.exports = sequelize;
