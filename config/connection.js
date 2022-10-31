e05ede57bae56d77fb8f99e310ed91187efebfbc
Christopher Dirksen <chrisdirksen024@outlook.com>
2022-10-31 13:03:08 -0400

    commit

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,12 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  
+
   {
     host: "localhost",
     dialect: "mysql",
     port: 3306,
   }
 );
+
+module.exports = sequelize;
