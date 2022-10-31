29c7bd94dce016fc7a08b71c898ada42f63794bc
Christopher Dirksen <chrisdirksen024@outlook.com>
2022-10-30 21:49:35 -0400

    commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
 
 
-const charRoutes = require('/characterRoutes');
+const charRoutes = require('./characterRoutes');
 router.use('/Character', characterRoutes );
 
-const classRoutes = require('/classRoutes');
-router.use('./Class', classRoutes);
+const classRoutes = require('./classRoutes');
+router.use('/Class', classRoutes);
 
-const inventoryRoutes = require('/inventoryRoutes');
+const inventoryRoutes = require('./inventoryRoutes');
 router.use('Inventory', inventryRoutes);
 
-const partyRoutes = require('/partyRoutes');
+const partyRoutes = require('./partyRoutes');
 router.use('/Party', partyRoutes);
 
-const raceRoutes = require('/raceRoutes');
+const raceRoutes = require('./raceRoutes');
 router.use('/Race', raceRoutes);
 
-const userRoutes = require('/userRoutes');
+const userRoutes = require('./userRoutes');
 router.use('/User', userRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const hbs = exphbs.create({});
-const router = require('express').Router();
-const { Pool } = require('pg');
+const path = require("path");
+const express = require("express");
+const router = require("express").Router();
+const sequelize = require('./controllers')
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server running at localhost:${PORT}`));
-});
+
