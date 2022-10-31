b6e5016eca22ff159512edca07e73f0ebd0ef2de
Joseph Lamb <jlambpco@gmail.com>
2022-10-31 10:57:56 -0400

    push

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,10 @@
 const router = require("express").Router();
-const characterRoutes = require('./characterRoutes');
-const classRoutes = require('./classRoutes');
-const historyRoutes = require('./historyRoutes');
-const inventoryRoutes = require('./inventoryRoutes');
-const partyRoutes = require('./partyRoutes');
-const raceRoutes = require('./raceRoutes');
-const userRoutes = require('./userRoutes');
+const characterRoutes = require("./characterRoutes");
+const partyRoutes = require("./partyRoutes");
+const userRoutes = require("./userRoutes");
 
-router.use('/', characterRoutes);
-router.use('/', classRoutes);
-router.use('/', historyRoutes);
-router.use('/', inventoryRoutes);
-router.use('/', partyRoutes);
-router.use('/', raceRoutes);
-router.use('/', userRoutes);
+router.use("/", characterRoutes);
+router.use("/", partyRoutes);
+router.use("/", userRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-
 const path = require("path");
 const express = require("express");
 const router = require("express").Router();
-const sequelize = require('./controllers');
-const exphbs = require('express-handlebars');
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
 
 const app = express();
-const {engine} = require('express-handlebars');
+const { engine } = require("express-handlebars");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.get('/', (req, res) => {
-  res.render('index', { 
-    title: 'PageofHolding',
-    style: 'home.css',
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "PageofHolding",
+    style: "home.css",
   });
 });
 
-app.get('/userLogin', (req, res) => {
-  res.render('userLogin', { 
-    title: 'User Login',
-    style: 'user.css'
-  })
-})
-
+app.get("/userLogin", (req, res) => {
+  res.render("userLogin", {
+    title: "User Login",
+    style: "user.css",
+  });
+});
 
 const PORT = process.env.PORT || 3030;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+});
