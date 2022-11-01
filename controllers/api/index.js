df94747e4f2c87bd8a8ae386a9bf9890f7513baf
Joseph Lamb <jlambpco@gmail.com>
2022-11-01 01:05:37 -0400

    final countdown

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 
+const characterRoutes = require("./characterRoutes");
+const userRoutes = require("./userRoutes");
+const partyRoutes = require("./partyRoutes");
+
+router.use("./character", characterRoutes);
+router.use("./user", userRoutes);
+router.use("./party", partyRoutes);
+
 module.exports = router;
