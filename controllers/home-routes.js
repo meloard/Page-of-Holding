7993ff852931dc3515b629b026721028ced60bd6
Joseph Lamb <jlambpco@gmail.com>
2022-11-01 00:25:18 -0400

    push

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,16 +5,21 @@ router.get("/", async (req, res) => {
   res.render("homepage");
 });
 
+router.get("/user", async (req, res) => {
+  res.render("characters");
+});
+
 router.get("/user/:id", async (req, res) => {
   try {
-    const currChar = await Character.findByPk(req.param.id);
+    const currChar = await Character.findByPk(req.param.id),
+    {where: active=true}
     res.render("charMain", currChar);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/user/:id", async (req, res) => {
+router.get("/users/:id", async (req, res) => {
   try {
     const charList = await Character.findByPk(req.params.id);
     const characters = charList.get({ plain: true });
